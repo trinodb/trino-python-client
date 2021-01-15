c4bac7903893a63cb6627e9cba7872d417bdc19c
Yukihiro Okada <yukihiro.okada@treasure-data.com>
2021-01-15 19:22:55 +0900

    Remove mock from tests_require list

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ kerberos_require = ["requests_kerberos"]
 
 all_require = [kerberos_require]
 
-tests_require = all_require + ["httpretty", "pytest", "pytest-runner", "mock", "pytz", "flake8"]
+tests_require = all_require + ["httpretty", "pytest", "pytest-runner", "pytz", "flake8"]
 
 setup(
     name="trino",
